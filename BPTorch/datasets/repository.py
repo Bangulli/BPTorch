e979a1ae32094c109b0e9b124daab88110845c6a
Bangulli <lorenz.achim.kuhn@gmail.com>
2026-04-02 09:33:46 +0000

    fix typo

--- BPTorch/datasets/repository.py
+++ BPTorch/datasets/repository.py
@@ -72,7 +72,7 @@ class BigPictureRepository(tc.utils.data.Dataset):
     
     def __len__(self):
         if self.return_type in ['wsi', 'path']:return len(self.imgs)
-        elif self.patches_prepared is None and self.return_type=='patch': return len(self.patch_idx)
+        elif self.prepatched_source is None and self.return_type=='patch': return len(self.patch_idx)
         elif (self.prepatched_source is not None) and self.return_type=='patch': return len(self.patch_paths)
     
     def __getitem__(self, idx):
